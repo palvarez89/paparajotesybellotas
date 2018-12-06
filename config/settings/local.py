173fba5a83d233f26788b5b0b06f10912263773a
Pedro Alvarez <palvarez89@gmail.com>
2018-12-06 00:09:38 +0000

    Sqlite for local development

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,6 +14,17 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
+# DATABASES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'weddb',
+    }
+}
+
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
