4ce77b1f393588035cb1bd69da1296e5dd21be2f
Pedro Alvarez <palvarez89@gmail.com>
2018-12-23 16:50:03 +0000

    Add www subdomain too

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,7 +27,7 @@ else:
 
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['paparajotesybellotas.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['www.paparajotesybellotas.com', 'paparajotesybellotas.com'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
