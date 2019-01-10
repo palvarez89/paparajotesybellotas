282ce05d781208512933d3ac33f569e4da7b1c89
Pedro Alvarez <palvarez89@gmail.com>
2019-01-10 00:05:03 +0000

    Make images lookup work in production

--- paparajotes_y_bellotas/views.py
+++ paparajotes_y_bellotas/views.py
@@ -4,8 +4,9 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.shortcuts import render_to_response
 
 def location_view(request):
+    dirpath = os.path.dirname(os.path.realpath(__file__))
     base_images_path = "images/venue/"
-    images_path = os.path.join("static", base_images_path)
+    images_path = os.path.join(dirpath, "static", base_images_path)
 
     all_images = sorted(os.listdir(images_path))
     all_images_full = [urllib.parse.urljoin(base_images_path, img) for img in all_images]
