205583f9381b206b80540e76ae9c4fbd07061828
Pedro Alvarez <palvarez89@gmail.com>
2019-06-04 22:40:23 +0100

    Add invitado to admin view

--- paparajotes_y_bellotas/users/admin.py
+++ paparajotes_y_bellotas/users/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
 from paparajotes_y_bellotas.users.forms import UserChangeForm, UserCreationForm
+from paparajotes_y_bellotas.users.models import Invitado
 
 User = get_user_model()
 
@@ -15,3 +16,6 @@ class UserAdmin(auth_admin.UserAdmin):
     fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+
+admin.site.register(Invitado)
