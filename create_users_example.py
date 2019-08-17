4ef910bfa309859827bb83b8ee94827bcd04cf28
Pedro Alvarez <palvarez89@gmail.com>
2019-08-17 13:37:46 +0100

    create_users_example.py: Read yaml file from args

--- create_users_example.py
+++ create_users_example.py
@@ -1,6 +1,7 @@
 import django
 import os
 import yaml
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
@@ -8,7 +9,16 @@ django.setup()
 from django.contrib.auth.hashers import make_password
 from paparajotes_y_bellotas.users import models
 
-with open("users.yaml", 'r') as f:
+args = sys.argv
+nargs = len(args)
+
+if (nargs != 2):
+    print("create_users_example.py <yamlfile>")
+    sys.exit(2)
+
+print(args)
+
+with open(args[1], 'r') as f:
     try:
         file_contents = yaml.safe_load(f)
     except Exception as e:
