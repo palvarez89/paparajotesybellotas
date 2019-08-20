e73e4f2ad5355a80aba1987d1e19018848fa738c
Pedro Alvarez <palvarez89@gmail.com>
2019-08-20 09:42:38 +0100

    views: Use render instead of render_to_response
    Because of this the csrf token was missing

--- paparajotes_y_bellotas/views.py
+++ paparajotes_y_bellotas/views.py
@@ -3,7 +3,7 @@ import random
 import urllib
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.contrib.messages import get_messages
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 from paparajotes_y_bellotas.users import models
 
@@ -16,7 +16,7 @@ def location_view(request):
     all_images = sorted(os.listdir(images_path))
     all_images_full = [urllib.parse.urljoin(base_images_path, img) for img in all_images]
 
-    return render_to_response('pages/location.html', {'request': request, 'venue_images': all_images_full, 'messages': storage})
+    return render(request, 'pages/location.html', {'venue_images': all_images_full, 'messages': storage})
 
 def homepage_view(request):
     query_results = models.User.objects.all()
@@ -25,4 +25,4 @@ def homepage_view(request):
     quotes = [{'mensaje': u.mensaje, 'firma': u.firma} for u in query_results if len(u.mensaje) > 4]
 
     random.shuffle(quotes)
-    return render_to_response('pages/home.html', {'request': request, 'quotes': quotes, 'messages': storage})
+    return render(request, 'pages/home.html', {'quotes': quotes, 'messages': storage})
